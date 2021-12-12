c72ca4548fbc18a5058edad614f044f92a7328bd
Osama Aburideh <x00154068@mytudublin.ie>
2021-12-12 20:15:08 +0000

    final commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing", {
+                            strings: ["Student", "Software Developer", "Web Developer", "System Analyst", "Cloud Engineer", "Project Manager"],
+                            typeSpeed: 100,
+                            backSpeed: 100,
+                            loop: true
+                            
+                        
+                            
+                    });
+                });
+function formResponse() {
+                        alert("Thank you, Your message is sent. I will get back to you as soon as possible.");
+}
+
